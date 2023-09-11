453e8b7de14f01b3c416dc7e420e9441a95b077c
Nicolas dos Santos <ndossantos@microsoft.com>
2023-09-11 09:12:00 -0400

    Initial Components

---
+++ src/Components/DigitalGuidebook/DigitalGuidebook.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function DigitalGuideBook() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Coming Soon</h1>
+      </header>
+    </div>
+  );
+}
+
+export default DigitalGuideBook;
