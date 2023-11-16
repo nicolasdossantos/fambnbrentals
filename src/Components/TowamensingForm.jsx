41eaeafd5c720a1b2a59baa69f6b249ceaacc81e
Nicolas dos Santos <nickdossantos@nicolass-mbp.mynetworksettings.com>
2023-11-16 15:43:02 -0500

    Form mostly done

--- src/Components/TowamensingForm.jsx
+++ src/Components/TowamensingForm.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import SignaturePad from 'react-signature-canvas';
 import { Box, TextField, Button, IconButton, Typography, Grid, Container } from '@mui/material';
@@ -19,6 +19,8 @@ export default function TowamensingForm() {
   const [lastNameState, setLastNameState] = useState(lastName || '');
   const [startDateState, setStartDateState] = useState(startDate || '');
   const [endDateState, setEndDateState] = useState(endDate || '');
+  const [signature, setSignature] = useState('');
+  const [initials, setInitials] = useState('');
   const [formData, setFormData] = useState({});
 
 
@@ -34,10 +36,40 @@ export default function TowamensingForm() {
   const ownerPhoneNumber = '267-721-0098';
   const lotNumber = '1352';
   const houseNumber = '194';
-  const houseStreet = 'Bison Circle';
+  const houseStreet = 'Bishop Circle';
 
   const cleaningCrewName = 'Gena O\'Connor';
   const cleaningCrewPhoneNumber = '570-926-0282';
+
+  useEffect(() => {
+    let isSubscribed = true; // flag to keep track of whether the component is mounted
+  
+    async function fetchSecrets() {
+      try {
+        const response = await fetch('https://fambnbbackend.azurewebsites.net/api/getSecrets?code=X8ZC7UFwAIHl16u5OoreMM8QT9AdDX0W7IyhVjMbuqOHAzFuwkUMnw==', {
+          method: 'POST',
+          body: JSON.stringify({ type: 'SIGNATURE-SAS' }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+  
+        if (!isSubscribed) return; // if component has unmounted, don't do anything else
+  
+        const data = await response.text();
+        getSignatures(data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  
+    fetchSecrets();
+  
+    return () => {
+      isSubscribed = false; // set the flag to false when the component unmounts
+    };
+  }, []);
+
 
 
   const handleCarChange = (index, event) => {
@@ -88,8 +120,9 @@ export default function TowamensingForm() {
       guestState: state,  // STATE
       cars, // FOR EACH CAR -> STATE + PLATE #
       guestSignature: signatureData, // SIGNATURE OF STR
-      ownerInitialsPath, //(PROPERTY OWNER/AGENT MUST INITIAL...)
-      ownerSignaturePath, // SIGNATURE OF PROPERTY OWNER/AGENT
+      initials, //(PROPERTY OWNER/AGENT MUST INITIAL...)
+      signature, // SIGNATURE OF PROPERTY OWNER/AGENT,
+      guestZip: zip, // ZIP
       ownerName, // NAME OF PROPERTY OWNER
       ownerPhoneNumber, // FOR QUESTIONS/COMPLAINTS PLEASE CONTACT
       lotNumber, // ACCT/LOT #
@@ -104,23 +137,22 @@ export default function TowamensingForm() {
     try {
       const response = await fetch('https://fambnbbackend.azurewebsites.net/api/getSecrets?code=X8ZC7UFwAIHl16u5OoreMM8QT9AdDX0W7IyhVjMbuqOHAzFuwkUMnw==', {
         method: 'POST',
-        body: JSON.stringify({type: 'SAS'}),
+        body: JSON.stringify({type: 'PDF-SAS'}),
         headers: {
           'Content-Type': 'application/json'
         }
       });
-      setShouldShowForm(false);
+     
 
-      // if (!response.ok) {
-      //   
-      //   throw new Error(`HTTP error! status: ${response.status}`);
-      // }
+      if (!response.ok) {
+        
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-      // const sasToken  = await response.text();
-      // console.log(sasToken);
-      // generatePDFBlob(formData).then((pdfBlob) => {
-      //   uploadPDF(pdfBlob, sasToken, `${firstNameState}_${lastNameState}_${startDateState}_${endDateState}.pdf`);
-      // });
+      const sasToken  = await response.text();
+      generatePDFBlob(formData).then((pdfBlob) => {
+        uploadPDF(pdfBlob, sasToken, `${firstNameState}_${lastNameState}_${startDateState}_${endDateState}.pdf`);
+      });
 
       // You might want to do something upon success, e.g., redirect or show a message
     } catch (error) {
@@ -135,12 +167,36 @@ export default function TowamensingForm() {
 
   };
 
+  //get images from blob storage
+  const getSignatures = async (sasToken) => { 
+    const url = `https://fambnbstorage.blob.core.windows.net/signatures/signature.png?${sasToken}`;
+    const url2 = `https://fambnbstorage.blob.core.windows.net/signatures/initials.png?${sasToken}`;
+    try {
+      const response = await fetch(url);
+      const response2 = await fetch(url2);
+      
+      if (!response.ok) throw new Error(`Failed to get signature image: ${response.statusText}`);
+      if (!response2.ok) throw new Error(`Failed to get initials image: ${response2.statusText}`);
+      
+      const image = await response.blob();
+      const image2 = await response2.blob();
+      
+      setSignature(image);
+      setInitials(image2);
+  
+    } catch (error) {
+      console.error('Failed to get image:', error);
+      // Handle errors here
+    }
+  };
+
   const generatePDFBlob = async (formData) => {
     // Create a container for the off-screen rendering
     const container = document.createElement('div');
-    container.style.height = '100vh';
-    container.style.marginTop = '20vh';
-    // container.style.left = '-9999px'; // Move off-screen
+    container.style.position = 'absolute';
+    container.style.height = '279.4mm';
+    container.style.width = '215.9mm';
+    container.style.left = '-9999px'; // Move off-screen
     document.body.appendChild(container);
   
     // Render your custom component inside the container
@@ -150,14 +206,29 @@ export default function TowamensingForm() {
     await new Promise((resolve) => setTimeout(resolve, 0));
   
     // Convert the rendered component to canvas and then to a PDF
-    const canvas = await html2canvas(container);
-    const imgData = canvas.toDataURL('image/png');
+    const canvas = await html2canvas(container, {
+      width: container.offsetWidth,
+      height: container.offsetHeight,
+      scale: 2, // Adjust scale as needed
+      useCORS: true, // If you're loading images from external URLs
+      onclone: (document) => {
+        // Ensure the cloned document will be visible for html2canvas
+        document.body.style.visibility = 'visible';
+      }
+    });
+    const pdfWidth = 215.9;
+    const pdfHeight = 279.4;
+  
+    // Create a jsPDF instance with the correct dimensions
     const pdf = new jsPDF({
       orientation: 'portrait',
       unit: 'mm',
-      format: 'a4',
+      format: [pdfWidth, pdfHeight]
     });
-    pdf.addImage(imgData, 'PNG', 0, 0);
+  
+    // Add the image to the jsPDF instance
+    pdf.addImage(canvas.toDataURL('image/jpeg', 1.0), 'JPEG', 0, 0, pdfWidth, pdfHeight);
+  
     const pdfBlob = pdf.output('blob');
   
     // Clean up: remove the off-screen container
@@ -167,29 +238,6 @@ export default function TowamensingForm() {
     return pdfBlob;
   };
 
-  // const uploadPDF = async (pdfBlob, sasToken) => {
-  //   const blobName = 'example.pdf'; // Replace with your actual blob name
-  //   const url = `https://fambnbstorage.blob.core.windows.net/pdf-container/${blobName}${sasToken}`;
-  
-  //   try {
-  //     const response = await fetch(url, {
-  //       method: 'PUT',
-  //       headers: {
-  //         'x-ms-blob-type': 'BlockBlob',
-  //         'Content-Type': 'application/pdf',
-  //         'Content-Length': pdfBlob.size
-  //       },
-  //       body: pdfBlob
-  //     });
-  
-  //     if (!response.ok) throw new Error(`Failed to upload PDF: ${response.statusText}`);
-  //     console.log('PDF uploaded to blob storage.');
-  //     // Handle successful upload here
-  //   } catch (error) {
-  //     console.error('Failed to upload PDF:', error);
-  //     // Handle errors here
-  //   }
-  // };
 
 const uploadPDF = async (pdfBlob, sasTokenBase, blobName) => {
   const url = `https://fambnbstorage.blob.core.windows.net/pdf-container/${blobName}?${sasTokenBase}`;
@@ -215,7 +263,7 @@ const uploadPDF = async (pdfBlob, sasTokenBase, blobName) => {
 
 
 
-  return ( <>{ shouldShowForm ?
+  return (
     <Container maxWidth="md">
       <Box component="form" onSubmit={handleSubmit} noValidate autoComplete="off" sx={{ mt: 4 }}>
         <Typography variant="h6">Personal Information</Typography>
@@ -298,7 +346,7 @@ const uploadPDF = async (pdfBlob, sasTokenBase, blobName) => {
         </Grid>
 
 
-        <Typography variant="h6" sx={{ mt: 4 }}>Car Information</Typography>
+        <Typography variant="h6" sx={{ mt: 4 }}>Car Information (Please add all cars that will be parked at the house)</Typography>
         {cars.map((car, index) => (
           <Box key={index} sx={{ mb: 2 }}>
             <TextField
@@ -333,6 +381,12 @@ const uploadPDF = async (pdfBlob, sasTokenBase, blobName) => {
         >
           Add Another Car
         </Button>
+        <div             style={{width: '100%', textAlign: 'center', borderRadius: '4px', backgroundColor: 'rgb(229, 246, 253)', padding: '20px'}}
+>
+          <div 
+          >I AFFIRM I HAVE READ THE CURRENT RULES OF CONDUCT AND RENTAL POLICY AND I AM AWARE OF THE TRASH COMPACTOR HOURS UPON CHECKOUT TIME.</div>
+          <a style={{color: 'blue'}} href="https://www.towamensing.com/documents2010/rules%20of%20conduct%202010.pdf" target='_blank'>Towamensing Trails Rules of Conduct</a>
+        </div>
         {/* Car inputs and buttons here, wrapped in Grid components as above */}
         <Typography variant="h6" sx={{ mt: 4 }}>Signature</Typography>
         <SignaturePad
@@ -341,12 +395,13 @@ const uploadPDF = async (pdfBlob, sasTokenBase, blobName) => {
           onEnd={onSignatureChange}
         // other props you might need
         />
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
-          <Button variant="contained" onClick={clearSignature}>Clear</Button>
+        <Box sx={{ display: 'flex', justifyContent: 'right' }}>
+          <Button variant="contained" style={{ backgroundColor: '#7AC7C4' }} onClick={clearSignature}>Clear Signature</Button>
         </Box>
-
-        <Button type="submit" variant="contained" sx={{ mt: 3 }}>Submit</Button>
+        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+        <Button type="submit" variant="contained" style={{ backgroundColor: '#7AC7C4' }} sx={{ mt: 3, px: 5, mb: 3}}>Submit</Button>
+        </Box>
       </Box>
-    </Container> : <InvisibleComponent formData={formData} /> } </>
+    </Container>
   );
 }
